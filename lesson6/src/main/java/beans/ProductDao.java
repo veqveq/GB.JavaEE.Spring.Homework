b813d921244c80f812e0a81266048a1bf3ee20c7
vasyakov <vasyakov94@gmail.com>
2020-12-23 15:42:18 +0300

    Improve code

--- lesson6/src/main/java/beans/ProductDao.java
+++ lesson6/src/main/java/beans/ProductDao.java
@@ -1,54 +1,17 @@
 package beans;
 
 import models.Product;
-import org.hibernate.Session;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.NoResultException;
-import java.util.List;
-
 @Component
-public class ProductDao {
-    private ShopSessionsFactory factory;
+public class ProductDao extends BasicDao<Product> {
 
     public ProductDao(ShopSessionsFactory factory) {
-        this.factory = factory;
+        super(factory);
     }
 
-    public Product findById(long id, Session session){
-        Product product = session.get(Product.class,id);
-        if (product == null){
-            throw new RuntimeException("Product not found");
-        }else{
-            return product;
-        }
-    }
-
-    public List<Product> findAll(Session session) {
-        List<Product> products = session.createQuery(("FROM Product"),Product.class).getResultList();
-        if (products == null) {
-            throw new RuntimeException("Products not found");
-        } else {
-            return products;
-        }
-    }
-
-    public void deleteById(long id){
-        try(Session session = factory.getCurrentSession()){
-            session.beginTransaction();
-            Product product = session.get(Product.class,id);
-            session.delete(product);
-            session.getTransaction().commit();
-        }catch (NoResultException e){
-            throw new RuntimeException("Client not found");
-        }
-    }
-
-    public void saveOrUpdate(Product product){
-        try(Session session = factory.getCurrentSession()){
-            session.beginTransaction();
-            session.saveOrUpdate(product);
-            session.getTransaction().commit();
-        }
+    @Override
+    public void init() {
+        cls = Product.class;
     }
 }
